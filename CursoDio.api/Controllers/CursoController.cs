db2e8c2c1ee7721d0165833c306a3aa881b33471
Pedro Ivo <engseg.pedroivo@gmail.com>
2021-10-14 08:58:25 -0300

    Implementado autenticacao de usuario

---
+++ CursoDio.api/Controllers/CursoController.cs
@@ -0,0 +1,43 @@
+﻿using CursoDio.api.Models.Cursos;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CursoDio.api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/cursos")]
+    [Authorize ]
+    public class CursoController : Controller
+    {
+        [SwaggerResponse(statusCode: 201, description: "Sucesso ao Cadastrar um curso")]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Post(CursoViewModelInput cursoViewModelInput)
+        {
+            var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            return Created("", cursoViewModelInput);
+        }
+        [SwaggerResponse(statusCode: 201, description: "Sucesso ao Obter um curso")]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Get()
+        {
+            var cursos = new List<CursoViewModelOutput>();
+            //var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            cursos.Add(new CursoViewModelOutput()
+            {
+                Login = "",
+                Descricao = "teste",
+                Nome = "teste"
+            });
+            return Ok(cursos);
+        }
+    }
+
+}
--- CursoDio.api/Controllers/UsuarioController.cs
+++ CursoDio.api/Controllers/UsuarioController.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CursoDio.api.Models.Usuarios;
-using CursoDio.api.Filters;
-using Swashbuckle.AspNetCore.Annotations;
+﻿using CursoDio.api.Filters;
 using CursoDio.api.Models;
+using CursoDio.api.Models.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace CursoDio.api.Controllers
 {
@@ -22,7 +23,36 @@ namespace CursoDio.api.Controllers
         [ValidacaoModelStateCustomizado]
         public IActionResult Logar(LoginViewModelInput loginViewModelInput)
         {
-            return Ok(loginViewModelInput);
+            var usuarioViewModelOutput = new UsuarioViewModelOutput()
+            {
+                Codigo = 1,
+                Login = "leandro",
+                Email = "email@email.com"
+            };
+            var secret = Encoding.ASCII.GetBytes("MzfsT&d9gprP>!9$Es(X!5g@;ef!5sbk:jH\\2.}8ZP'qY#7");
+           var symmetricSecurityKey = new SymmetricSecurityKey(secret);
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, usuarioViewModelOutput.Codigo.ToString()),
+                    new Claim(ClaimTypes.Name, usuarioViewModelOutput.Login.ToString()),
+                    new Claim(ClaimTypes.Email, usuarioViewModelOutput.Email.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var tokeGenerated = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            var token = jwtSecurityTokenHandler.WriteToken(tokeGenerated);
+
+
+            return Ok(new
+            {
+                Token = token,
+                Usuario = usuarioViewModelOutput
+            }
+                ); 
         }
         [HttpPost]
         [Route("registrar")]
